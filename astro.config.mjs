b700cd3ba7eb728410df95665570b21316ed5db1
Bashar al-Idreesi <basharalidreesi@pc-143.home>
2025-07-21 21:28:07 +0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://basharalidreesi.github.io',
+    base: 'pomodoro-timer',
+});
